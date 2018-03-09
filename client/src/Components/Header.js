3f2cb2d0e823bea14b1f1ef9a10df8467013cd08
Thomas Laird <t6r6l5@gmail.com>
2018-03-09 14:38:25 -0700

    simple header

---
+++ client/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.png';
+import './Styles/Header.css';
+
+const Header = ({ user }) => {
+  return (
+    <header className="App-header">
+      <img src={logo} alt="logo" />
+      <a className="back" href="https://t-laird.com">Back to t-laird.com</a>
+    </header>
+  );
+}
+
+export default Header;
