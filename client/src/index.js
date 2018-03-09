194ab7ab6836ecc664ee26b458488077f6eded09
Thomas Laird <t6r6l5@gmail.com>
2018-03-09 14:38:11 -0700

    default index.js with router

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+
+const router = 
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+
+ReactDOM.render(router, document.getElementById('root'));
+registerServiceWorker();
